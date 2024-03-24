d91624c20ab184ef51c8ba4e9ea095058c1a939e
unknown <aramdeveloper1@gmail.com>
2024-03-24 15:10:26 +0300

    commit

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  // iterate from the beginning of the string to the middle of the string
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
 
-/* 
-  Add your pseudocode here
-*/
+  // if we reach the middle, and all the letters match, return true
+  return true;
+}
+// iterate from the beginning of the string to the middle of the string
+//   compare the letter we're iterating over to the corresponding letter at the end of the string
+// if the letters don't match, return false
+
+// if we reach the middle, and all the letters match, return true
 
 /*
   Add written explanation of your solution here
